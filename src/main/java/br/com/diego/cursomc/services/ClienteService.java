0f547b7922cfe07d4e5cac8934aa8cb2c29d82f6
Diego Marques <fariia_di@hotmail.com>
2020-04-26 15:58:17 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/br/com/diego/cursomc/services/ClienteService.java
+++ src/main/java/br/com/diego/cursomc/services/ClienteService.java
@@ -9,25 +9,42 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.diego.cursomc.domain.Cidade;
 import br.com.diego.cursomc.domain.Cliente;
+import br.com.diego.cursomc.domain.Endereco;
+import br.com.diego.cursomc.domain.enums.TipoCliente;
 import br.com.diego.cursomc.dto.ClienteDTO;
+import br.com.diego.cursomc.dto.ClienteNewDTO;
 import br.com.diego.cursomc.repositories.ClienteRepository;
+import br.com.diego.cursomc.repositories.EnderecoRepository;
 import br.com.diego.cursomc.services.exception.DataIntegrityException;
 import br.com.diego.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired // instaciada automaticamente pelo Spring
 	private ClienteRepository repo;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -38,8 +55,7 @@ public class ClienteService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
 		}
 	}
@@ -55,6 +71,21 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
