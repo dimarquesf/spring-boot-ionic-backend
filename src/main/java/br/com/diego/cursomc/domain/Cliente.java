28be19dc3348b137a3d9f299b7047a0c212b8a64
Diego Marques <fariia_di@hotmail.com>
2020-04-28 07:34:14 -0300

    Salvando Perfis de usuario na base de dados

--- src/main/java/br/com/diego/cursomc/domain/Cliente.java
+++ src/main/java/br/com/diego/cursomc/domain/Cliente.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,6 +20,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.diego.cursomc.domain.enums.Perfil;
 import br.com.diego.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -45,12 +48,16 @@ public class Cliente  implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>(); //Set não tem repetição
 	
+	@ElementCollection(fetch=FetchType.EAGER) //eager =  chamar os clientes trazeer tbm os perfis 
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore // Pedidos de Cliente não vai ser Serializado
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-		
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -61,6 +68,7 @@ public class Cliente  implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -109,6 +117,14 @@ public class Cliente  implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis(){
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet()); //Retornar os Perfils de Cliente
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
 	}
 
 	public List<Endereco> getEnderecos() {
