328b6ad5a9e4c421dc1c73cf715af334cd6290c9
Diego Marques <fariia_di@hotmail.com>
2020-05-01 20:01:05 -0300

    Usando padrao de nomes para imagens

---
+++ src/main/java/br/com/diego/cursomc/services/ImageService.java
@@ -0,0 +1,54 @@
+package br.com.diego.cursomc.services;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.diego.cursomc.services.exception.FileException;
+
+@Service
+public class ImageService {
+
+	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
+		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename()); //pegar extensão do arquivo
+		if (!"png".equals(ext) && !"jpg".equals(ext)) { // se a extensão não for png ou jpg
+			throw new FileException("Somente imagens PNG e JPG são permitidas"); // rejeitada
+		}
+
+		try {
+			BufferedImage img = ImageIO.read(uploadedFile.getInputStream()); // ler a imagem do arquivo
+			if ("png".equals(ext)) {
+				img = pngToJpg(img); //converter de png para jpg
+			}
+			return img;
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+
+	public BufferedImage pngToJpg(BufferedImage img) {
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
+		return jpgImage;
+	}
+
+	public InputStream getInputStream(BufferedImage img, String extension) {
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(img, extension, os);
+			return new ByteArrayInputStream(os.toByteArray());
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+}
